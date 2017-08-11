9581c966b2a32b13fb533bd1db53aef1575abd24
Denis Rykovanov <drykovanov@wiley.com>
2017-08-11 08:58:55 +0300

    [+] init commit

---
+++ Controller.h
@@ -0,0 +1,24 @@
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
+
+
+#include <Arduino.h>
+#include <MovingPlatform.h>
+
+
+
+class Controller {
+private:
+  MovingPlatform platform;
+public:
+  Controller(MovingPlatform& aPlatform) : platform{aPlatform} {
+  }
+
+  void setup();
+  void onTick();
+
+};
+
+
+
+#endif /* end of include guard: _CONTROLLER_H_ */
